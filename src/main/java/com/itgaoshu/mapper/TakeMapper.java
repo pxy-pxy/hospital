fd0fa4cd4c5e9edfa9f3d879138e54ae1c3c96c2
Tan30123 <3012388504@qq.com>
2020-12-23 19:33:58 +0800

    Merge branch 'master' of https://github.com/pxy-pxy/hospital
    # Conflicts:
    #	src/main/java/com/itgaoshu/Controller/FirstContorlller.java

---
+++ src/main/java/com/itgaoshu/mapper/TakeMapper.java
@@ -0,0 +1,22 @@
+package com.itgaoshu.mapper;
+
+import com.itgaoshu.bean.ReportVo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Tan
+ * @create 2020-12-23 18:44
+ */
+@Mapper
+public interface TakeMapper {
+    //查询挂号的所有信息用于页面展示
+    List<ReportVo> sel();
+    //药品出库修改用户状态
+    Integer chuku(ReportVo reportVo);
+    //查看所有交费出院的患者
+    List<ReportVo> selhuan(ReportVo reportVo);
+    //查询该用户的总价钱
+    int zong(Integer reid);
+}
