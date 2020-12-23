fb9565baaa0f09120ea03d024176a39874a75bd9
pxy-pxy <3065516116@qq.com>
2020-12-23 15:44:39 +0800

    查询仓库注释

--- src/main/java/com/itgaoshu/Controller/WarehuoseController.java
+++ src/main/java/com/itgaoshu/Controller/WarehuoseController.java
@@ -7,6 +7,7 @@ import com.itgaoshu.service.WarehuoseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +22,8 @@ public class WarehuoseController {
     /*
      * 查询仓库
      * */
+    @RequestMapping("/findAllWarehuose")
+    @ResponseBody
     public Object findAllWarehuose(Warehuose warehuose ,Integer page,Integer limit){
         PageHelper.startPage(page,limit);
         List<Warehuose> list = warehuoseService.findAllWarehuose(warehuose);
@@ -34,7 +37,35 @@ public class WarehuoseController {
 
     }
     /*
-     * 查询仓库
+     * 添加仓库
      * */
+    @RequestMapping("/addWarehuose")
+    @ResponseBody
+    public  Object addWarehuose(Warehuose warehuose){
+        int count = warehuoseService.count(warehuose);
+        if (count==0){
+            int i = warehuoseService.addWarehuose(warehuose);
+            if (i==1){
+                return "添加成功";
+            }else {
+                return "添加失败";
+            }
+        }else {
+            return warehuose.getSupplierName()+"已存在";
+        }
+    }
+    /*
+    * 删除仓库
+    * */
+    @ResponseBody
+    public Object deleteWarehuose(Integer warehouseId){
+        int i = warehuoseService.deleteWarehuose(warehouseId);
+        if(i==1){
+            return "删除成功";
+        }else{
+            return "删除失败";
+        }
+    }
+
 
 }
