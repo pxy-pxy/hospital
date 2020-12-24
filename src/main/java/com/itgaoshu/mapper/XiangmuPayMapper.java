74915faaf972ad73408897a4e9f69b3ae9b1e69a
Tan30123 <3012388504@qq.com>
2020-12-24 09:10:05 +0800

    Merge branch 'master' of https://github.com/pxy-pxy/hospital
    # Conflicts:
    #	src/main/java/com/itgaoshu/Controller/FirstContorlller.java

--- src/main/java/com/itgaoshu/mapper/XiangmuPayMapper.java
+++ src/main/java/com/itgaoshu/mapper/XiangmuPayMapper.java
@@ -7,15 +7,15 @@ import java.util.List;
 
 /**
  * @author Tan
- * @create 2020-12-23 17:07
+ * @create 2020-12-23 22:37
  */
 @Mapper
 public interface XiangmuPayMapper {
-    //查询用户所有的项目
+    //查询用户所有项目
     List<Cashier> selxiang(Integer perid);
     //查询该用户有没有收费项目
     Integer selshoux(Integer perid);
-    //项目收费修改项目的收费状态
+    //项目收费后修改项目的收费状态
     Integer updxiang(Integer perid);
     //项目检查页面查询用户所有要检查的项目
     List<Cashier> selcha(Integer perid);
